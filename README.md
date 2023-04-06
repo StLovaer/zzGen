eded303da94d560737cb7d8879a01531a55d827b
Lirkrid <884507318@qq.com>
2023-04-06 15:10:11 +0800

    benchbase

---
+++ README.md
@@ -0,0 +1,35 @@
+## zzGen
+> ### 🔧 a simple SQL query generation tool.
+> ### ‘zz’ is the initial pinyin letter of the Chinese word “智障”.
+
+
+
+## Benchbase Quickstart
+
+In the `benchbase` folder, we have a tgz file, which can be extracted,
+
+```bash
+cd benchbase
+tar xvzf benchbase-postgres.tgz
+cd benchbase-postgres
+```
+
+Inside this folder, you can run BenchBase. For example, to execute the `tpcc` benchmark,
+
+```bash
+java -jar benchbase.jar -b tpcc -c config/postgres/sample_tpcc_config.xml --create=true --load=true --execute=true
+```
+
+**We modified the benchbase source code to output all the queries in the workload in text format when testing**. (Only seats, sibench, smallbank, tatp, tpcc, tpch are supported.)For example, after executing the `tpcc` benchmark, a  `tpcc_workload.txt` will be generated, each line is a query. 
+
+A full list of options can be displayed,
+
+```bash
+java -jar benchbase.jar -h
+```
+
+---
+
+
+
+java -jar benchbase.jar -b tpch -c config/postgres/sample_tpch_config.xml --create=true --load=true --execute=true
---
+++ tools.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+SUBJECT=lirk
+VERSION=1.0
+USAGE="Usage: command -hv args"
+
+# --- Option processing --------------------------------------------
+while getopts ":vh" optname
+  do
+    case "$optname" in
+      "v")
+        echo "Version $VERSION"
+        exit 0;
+        ;;
+      "h")
+        echo $USAGE
+        exit 0;
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        exit 0;
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
+shift $(($OPTIND - 1))
+
+cmd=$1
+param=$2
+command="command_$1"
+
+# -----------------------------------------------------------------
+LOCK_FILE=/tmp/${SUBJECT}.lock
+
+if [ -f "$LOCK_FILE" ]; then
+echo "Script is already running"
+exit
+fi
+
+# -----------------------------------------------------------------
+trap "rm -f $LOCK_FILE" EXIT
+touch $LOCK_FILE 
+
+# -----------------------------------------------------------------
+function command_test {
+    echo "test"
+}
+
+function command_tpch {
+    echo "-- generate tpch quries using template$param:"
+    cd ./tools/TPC-H/queries/
+    ./qgen -d $param
+}
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+if [ -n "$(type -t ${command})" ] && [ "$(type -t ${command})" = function ]; then 
+   ${command}
+else 
+   echo "'${cmd}' is NOT a command"; 
+fi
+
